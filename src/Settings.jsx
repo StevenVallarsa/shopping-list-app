cc4cb81c3257e1ee2350e76909bbfc5ca723b402
StevenVallarsa <orionsjv@gmail.com>
2023-10-21 20:19:39 -0400

    add select with onClick to test

--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 import groceryListItems from "./groceryListItems";
 import deptList from "./deptList";
 
 export default function Settings() {
+  const [groceries, setGroceries] = useState(groceryListItems);
+
+  const handleItemClick = id => {
+    setGroceries(prev =>
+      prev.map(item => {
+        if (item.id === id) {
+          return { ...item, isSelected: !item.isSelected };
+        } else return item;
+      })
+    );
+  };
+
   return (
     <>
       <h1>Modify List Items</h1>
@@ -9,10 +23,16 @@ export default function Settings() {
         <>
           <h2 key={dept}>{dept}</h2>
           <ul>
-            {groceryListItems
+            {groceries
               .filter(item => item.dept === dept)
               .map(product => (
-                <li key={product.id}>{product.name}</li>
+                <li
+                  key={product.id}
+                  style={{ fontWeight: product.isSelected ? "bold" : "", color: product.isSelected ? "red" : "black" }}
+                  onClick={() => handleItemClick(product.id)}
+                >
+                  {product.name}
+                </li>
               ))}
           </ul>
         </>
